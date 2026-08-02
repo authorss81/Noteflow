bd7cdd932bdf5c4099e338b5f62f7868e2660db8
SouvikSarkar011008 <souviksarkar011008@gmail.com>
2026-08-02 10:07:30 +0530

    Android: force compileSdk 36 for all plugin subprojects

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -19,6 +19,18 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+// Force a modern compileSdk for all plugin subprojects (some older plugins
+// hardcode compileSdk 34, but current Flutter plugins require 36+).
+subprojects {
+    afterEvaluate {
+        if (project.hasProperty("android")) {
+            project.extensions.configure<com.android.build.gradle.BaseExtension> {
+                compileSdkVersion(36)
+            }
+        }
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
