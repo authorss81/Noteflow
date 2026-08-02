3873fd7c61bf3946bcf99f6689617cbd744136e9
SouvikSarkar011008 <souviksarkar011008@gmail.com>
2026-08-02 09:55:55 +0530

    Android: pin compileSdk 36 (file_picker requires it)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.yourname.noteflow"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
