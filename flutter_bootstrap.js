16a34aac131337786b5398827432f90108202b80
authorss81 <authorss81@gmail.com>
2026-08-02 06:22:51 +0000

    Deploying to gh-pages from @ authorss81/Noteflow@31090c29a3ebb86e627ab264a393bf10b32af33e 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c00
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3044902203" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "331693050" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
